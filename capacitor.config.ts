e0dbcfe3c21b75c991fa4e0f9cbdabad22af1bc6
Francisco Reyna <francisco.reyna@grupolufesa.com>
2026-02-19 23:30:19 -0600

    Adding changes to splash screen, incos, and plugin

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,22 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'quetzart',
-  webDir: 'www'
+  appId: 'mx.quetzart.app',
+  appName: 'Quetzart',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      backgroundColor: "#ffffff",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP"
+    }
+  }
+  // webDir: 'www',
+  // "server": {
+  //   "url": "http://192.168.100.16:4200",
+  //   "cleartext": true
+  // },
 };
 
 export default config;
