262eaa7c14e791fc4a575fb7ff8e7dbbe508dc97
Francisco Reyna <francisco.reyna@grupolufesa.com>
2026-06-13 14:12:47 -0600

    version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://quetzartpi.gpolufesa.com',
-  appVersion: '0.0.0',
+  appVersion: '0.0.1',
   androidUpdateUrl: 'https://play.google.com/store/apps/details?id=mx.quetzart.app',
   iosUpdateUrl: 'https://apps.apple.com/search?term=Quetzart',
   appUpdateUrl: 'https://play.google.com/store/apps/details?id=mx.quetzart.app'
